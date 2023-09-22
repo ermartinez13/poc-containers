d85f4b0fa50766f2c934351a9e427fbe4f2b6307
Erick Martinez <ermartinez13@gmail.com>
2023-09-22 10:27:57 -0400

    scaffold gateway service

--- gateway/index.js
+++ gateway/index.js
@@ -1,11 +1,24 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 const port = 3000;
+const fooProxy = createProxyMiddleware({
+  target: "http://localhost:3001",
+  pathRewrite: {
+    "^/foo": "/",
+  },
+});
+const barProxy = createProxyMiddleware({
+  target: "http://localhost:3002",
+  pathRewrite: {
+    "^/bar": "/",
+  },
+});
 
-app.get("/foo", (req, res) => {});
+app.get("/foo", fooProxy);
 
-app.get("/bar", (req, res) => {});
+app.get("/bar", barProxy);
 
 app.listen(port, () => {
   console.log(`Gateway service listening at http://localhost:${port}`);
